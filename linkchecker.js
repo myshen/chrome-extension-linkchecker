3a13c1bcbf9142273c1f5157786771fcf2a73bf0
Matt Shen <synmantics@gmail.com>
2011-01-30 21:29:49 -0800

    Overhaul to use HTTP HEAD

--- linkchecker.js
+++ linkchecker.js
@@ -1,17 +1,10 @@
-String.prototype.startsWith = function(text) {
-  return this.substr(0, text.length) == text;
-};
-String.prototype.endsWith = function(text) {
-  return this.substr(-text.length) == text;
-};
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
     var animateSpeed = 600;
     var anchors = $("a");
     var checked = 0;
     var checkStatus = $("<div></div>")
-      .css({"width": "50%",
-            "position": "absolute", "top": "0"})
+      .css({"width": "50%", "position": "absolute", "top": 0})
       .hide()
       .progressbar({value: 0, change: function (event, ui) {
             if (checked == anchors.size()) {
@@ -22,39 +15,48 @@ chrome.extension.onRequest.addListener(
     $(".ui-progressbar-value", checkStatus)
       .css({"height": "100%",
             "background-image": "url(http://jqueryui.com/demos/progressbar/images/pbar-ani.gif)"});
-    function checkURL(url, link) {
-      chrome.extension.sendRequest({"action": "check", "url": url}, 
+    checkStatus.show(animateSpeed);
+    function checkedPlusPlus() {
+      checked++;
+      checkStatus.progressbar({"value": checked / anchors.size()*100});
+    }
+    function checkURI(uri, link) {
+      link.css("background-color", "#ffa");
+      chrome.extension.sendRequest({"action": "check", "uri": uri}, 
         function (response) {
           if (response) {
             if (200 <= response.status && response.status < 400) {
               link.css("background-color", "#afa");
             } else {
-              console.log("Failed link XHR", response, link[0]);
-              link.css("background-color", "#faa");
+              if (response.status == 0) {
+                // UNSENT, OPENED or error
+                console.log("XHR timed out or UNSENT/OPENED", link[0]);
+                link.css("background-color", "#aaf");
+              } else {
+                console.log("Failed link", response, link[0]);
+                link.css("background-color", "#faa");
+              }
             }
-            checked++;
-            checkStatus.progressbar({"value": checked / anchors.size()*100});
+            checkedPlusPlus();
           }
         });
     }
-    checkStatus.show(animateSpeed);
+    var base = new URI(window.location.href);
     anchors.each(function () {
       var link = $(this);
-      var url = link.attr('href');
-      
-      if (url && !url.startsWith("http")) {
-        if (window.location.href.endsWith("/") && url.startsWith("/")) {
-          url = window.location.href.slice(0, -1) + url;
-        } else {
-          url = window.location.href + url;
-        }
+      var uri = link.attr('href');
+
+      if (!uri) {
+        return checkedPlusPlus();
       }
 
-      // before send
-      link.css("background-color", "#ffa");
+      uri = new URI(uri).resolve(base);
 
-      checkURL(url, link);
+      if (uri.getScheme() == 'javascript') {
+        return checkedPlusPlus();
+      }
+      
+      checkURI(uri.toString(), link);
     });
-    //checkStatus.hide();
-    sendResponse({});
+    sendResponse();
   });
