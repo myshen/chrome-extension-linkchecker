c71fe18c9b06bc5c4ae05d5d80d2bfd13ee0eb07
Matt Shen <synmantics@gmail.com>
2011-07-18 23:16:59 -0700

    Prettify the status display and enable removing all markings.

--- linkchecker.js
+++ linkchecker.js
@@ -4,26 +4,46 @@ chrome.extension.onRequest.addListener(
     var anchors = $("a");
     var checked = 0;
     var bad = 0;
-    var checkStatus = $("<div></div>")
-      .css({"width": "50%", "position": "absolute", "top": 0})
+    var checkStatusHolder = $("<div></div>")
+      .css({"width": "50%", "position": "absolute",
+            "background": "#ffffff",
+            "top": 0, 'padding': '0.5em 1em'})
       .hide()
+      .hover(function () {
+        $(this).fadeTo(animateSpeed, 1);
+      }, function () {
+        $(this).fadeTo(animateSpeed, 0.3);
+      })
       .appendTo("body");
-    checkStatus.show(animateSpeed);
+    var checkStatus = $("<span></span>").appendTo(checkStatusHolder);
+    var closeButton = $('<button>Clear markings</button>').appendTo(checkStatusHolder)
+      .css('margin-left', '1em')
+      .click(function () {
+        checkStatusHolder.hide(animateSpeed).remove();
+        anchors.each(function () {
+          $(this).css('background-color', $(this).data('linkchecker_saved_bgc'));
+        });
+      });
+    checkStatusHolder.show(animateSpeed);
     function checkedPlusPlus() {
       checked++;
       var percent = Math.round(checked / anchors.size() * 100);
       checkStatus.html(percent + '%');
       if (percent >= 100) {
         if (bad > 0) {
-          checkStatus.css('background', '#f55');
-          checkStatus.html(bad + (bad == 1 ? 'error' : 'errors') +
-                           ' occurred while checking links');
+          checkStatusHolder.css('background', '#ff5555');
+          checkStatus.html(bad + ' ' + (bad == 1 ? 'error' : 'errors') +
+              ' occurred while checking links. Open the developer ' +
+              'tools (command-option-j/ctrl-shift-j) to see which ' + 
+              'links failed.');
         } else {
-          checkStatus.remove();
+          checkStatus.html('All links OK');
         }
+        checkStatusHolder.fadeTo('slow', 0.3);
       }
     }
     function checkURI(uri, link) {
+      link.data('linkchecker_saved_bgc', link.css('background-color'));
       link.css("background-color", "#ffa");
       chrome.extension.sendRequest({"action": "check", "uri": uri}, 
         function (response) {
